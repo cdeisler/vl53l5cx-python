502713ea95c8f4a8be28f13c5dee84d4ad1800b1
CJ Deisler <c33jd13s3l@gmail.com>
2023-04-17 21:56:53 -0400

    Update test.py

--- examples/test.py
+++ examples/test.py
@@ -4,7 +4,7 @@ import vl53l5cx_ctypes as vl53l5cx
 from vl53l5cx_ctypes import STATUS_RANGE_VALID, STATUS_RANGE_VALID_LARGE_PULSE
 
 print("Uploading firmware, please wait...")
-vl53 = vl53l5cx.VL53L5CX()
+vl53 = vl53l5cx.VL53L5CX(skip_init=False)
 print("Done!")
 vl53.set_resolution(8 * 8)
 vl53.enable_motion_indicator(8 * 8)
